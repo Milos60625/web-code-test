0f7ad5356c0fde36d4900055d09d033d30ac4415
Milos60625 <milosdjor011@gmail.com>
2021-02-23 23:53:09 -0500

    cart add and remove done, next up is total and doing quantity

--- src/BuyList.jsx
+++ src/BuyList.jsx
@@ -1,51 +1,120 @@
+import React from 'react';
+import { useCart } from './Cart';
+import { useDispatchCart } from './Cart'
 
 const BuyList = () => {
+  const items = useCart();
+  const dispatch = useDispatchCart();
+  const totalPrice = items.reduce((total,b) => total + b.price, 0)
+
+  const handleRemove = index => {
+      dispatch({type : 'REMOVE', index})
+  }
+  if (items.length === 0) {
+    return <main>Cart is empty</main>;
+  }
   return (
-  <div className="mt-4">
-    <h1 className="font-semibold text-2xl">Buy List</h1>
+    <main>
+      <p>
+        {items.map((item, index) => (
+          <div>
+            <h4 className="flex flex-1 items-center font-semibold leading-4 mt-2">
+              {item.name}
+            </h4>
+            <button
+              type="button"
+              key={index}
+              className="bg-red-100 hover:bg-red-200 p-1 rounded ml-2"
+              onClick={() => handleRemove(index)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="text-red-500 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </button>          </div>
+        ))}
+      </p>
+    </main>
+    // <div className="mt-4">
+    //   <h1 className="font-semibold text-2xl">Buy List</h1>
 
-    <div className="border border-gray-200 p-4 rounded shadow mt-2 grid grid-cols-1 gap-y-4">
-      {/*
-      ******************************
-      * TEMPLATE FOR BUY LIST ITEM *
-      ******************************
-      *
-      <div className="grid border border-gray-200 gap-x-4 rounded shadow p-4" style={{ gridTemplateColumns: "1fr 10fr 1fr 0fr 1fr 0fr 1fr 0fr"}}>
-        <img src="https://cdnimg.webstaurantstore.com/images/products/small/228802/1679695.jpg" alt="Refrigerator" className="p-2 rounded border border-gray-200" />
-        <div className="flex flex-col flex-grow justify-center">
-          <h3 className="font-semibold text-lg">Refrigerator</h3>
-          <p>
-            53" Black Sliding Glass Door Merchandiser Refrigerator with LED Lighting
-          </p>
-        </div>
+    //   <div className="border border-gray-200 p-4 rounded shadow mt-2 grid grid-cols-1 gap-y-4">
+    //     {/* ******************************
+    //      * TEMPLATE FOR BUY LIST ITEM *
+    //      ******************************
+    //      * */}
+    //     <div
+    //       className="grid border border-gray-200 gap-x-4 rounded shadow p-4"
+    //       style={{ gridTemplateColumns: '1fr 10fr 1fr 0fr 1fr 0fr 1fr 0fr' }}
+    //     >
+    //       <img
+    //         src="https://cdnimg.webstaurantstore.com/images/products/small/228802/1679695.jpg"
+    //         alt="Refrigerator"
+    //         className="p-2 rounded border border-gray-200"
+    //       />
+    //       <div className="flex flex-col flex-grow justify-center">
+    //         <h3 className="font-semibold text-lg">Refrigerator</h3>
+    //         <p>
+    //           53" Black Sliding Glass Door Merchandiser Refrigerator with LED
+    //           Lighting
+    //         </p>
+    //       </div>
 
-        <div className="font-semibold text-lg text-red-500 flex items-center justify-end">
-          $1,949.00
-        </div>
+    //       <div className="font-semibold text-lg text-red-500 flex items-center justify-end">
+    //         $1,949.00
+    //       </div>
 
-        <span className="flex items-center font-semibold">x</span>
+    //       <span className="flex items-center font-semibold">x</span>
 
-        <div className="flex items-center justify-center">
-          <input type="number" className="border border-gray-300 rounded w-24 text-lg px-2" value={1} />
-        </div>
+    //       <div className="flex items-center justify-center">
+    //         <input
+    //           type="number"
+    //           className="border border-gray-300 rounded w-24 text-lg px-2"
+    //           value={2}
+    //         />
+    //       </div>
 
-        <span className="flex items-center">=</span>
+    //       <span className="flex items-center">=</span>
 
-        <span className="flex items-center font-semibold text-green-500 text-xl">
-          $1,949.00
-        </span>
+    //       <span className="flex items-center font-semibold text-green-500 text-xl">
+    //         $1,949.00
+    //       </span>
 
-        <div className="flex items-center justify-end">
-          <button type="button" className="bg-red-100 hover:bg-red-200 p-1 rounded ml-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="text-red-500 w-6">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-            </svg>
-          </button>
-        </div>
-      </div> */}
-    </div>
-  </div>
-  )
-}
+    //       <div className="flex items-center justify-end">
+    //         <button
+    //           type="button"
+    //           className="bg-red-100 hover:bg-red-200 p-1 rounded ml-2"
+    //         >
+    //           <svg
+    //             xmlns="http://www.w3.org/2000/svg"
+    //             fill="none"
+    //             viewBox="0 0 24 24"
+    //             stroke="currentColor"
+    //             className="text-red-500 w-6"
+    //           >
+    //             <path
+    //               strokeLinecap="round"
+    //               strokeLinejoin="round"
+    //               strokeWidth={2}
+    //               d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+    //             />
+    //           </svg>
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+};
 
 export default BuyList;
--- src/StockProducts.jsx
+++ src/StockProducts.jsx
@@ -1,12 +1,20 @@
 import StockProductsData from "./data/stockProducts.json";
 import {useState}  from 'react'
+import { useDispatchCart } from './Cart'
 // console.log(StockProductsData)
 
 const StockProducts = () => {
-  const [cart, setCart] = useState([]);
-  console.log('cart', cart)
-  const addToCart = (stockProductData) => {
-    setCart([...cart, stockProductData])
+  const dispatch = useDispatchCart();
+  // const items = useCart();
+  // console.log('items', items)
+  // const [cart, setCart] = useState([]);
+  // console.log('cart', cart)
+  // const addToCart = (stockProductData) => {
+  //   setCart([...cart, stockProductData])
+  // }
+  const addToCart = item => {
+    console.log(item)
+    dispatch({ type : 'ADD', item})
   }
   return (
 
@@ -23,9 +31,11 @@ const StockProducts = () => {
           <img src={stockProductData.image} alt={stockProductData.name} className="block w-full rounded shadow p-2 bg-white" />
           <h4 className="flex flex-1 items-center font-semibold leading-4 mt-2">{stockProductData.name}</h4>
           <h4 className="flex flex-1 items-center font-semibold leading-4 mt-2">{stockProductData.price}$</h4>
-          <h4 className="flex flex-1 items-center font-semibold leading-4 mt-2">{stockProductData.description}</h4>
+          {/* <h4 className="flex flex-1 items-center font-semibold leading-4 mt-2">{stockProductData.description}</h4> */}
 
+          {/* <button type="button" className="absolute w-8 top-0.5 right-0.5 p-0.5 bg-white shadow rounded-full bg-green-100" onClick={() => addToCart(stockProductData)}> */}
           <button type="button" className="absolute w-8 top-0.5 right-0.5 p-0.5 bg-white shadow rounded-full bg-green-100" onClick={() => addToCart(stockProductData)}>
+
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(16, 185, 129)">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
